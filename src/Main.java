bb76b2fa3f29bb985819d5c6e8a55db162905c24
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-03-31 09:06:18 +0400

    Accessing on Circle class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter The Radius of the Circle :");
+        double r=scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Enter The Color of the Circle :");
+        String color=scanner.nextLine();
+        Circle circle=new Circle(r,color);
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Color: " + circle.getColor());
+        System.out.println("Area: " + circle.calculateCircleArea());
+
     }
 }
